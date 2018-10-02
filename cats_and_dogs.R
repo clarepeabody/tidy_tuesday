21de3fd10995d92d7b0310db77cb071c7ff9f0af
clarepeabody <clare.peabody@gmail.com>
2018-10-02 09:17:24 -0700

    R Studio commit 10/2

--- cats_and_dogs.R
+++ cats_and_dogs.R
@@ -1,1 +1,8 @@
-awesgd
+library(tidyverse)
+library(httr)
+library(readxl)
+
+GET("https://query.data.world/s/qage5daw5py3mwznddsotjbf2ewr64", write_disk(tf <- tempfile(fileext = ".xlsx")))
+pets <- read_excel(tf)
+
+head(pets)
