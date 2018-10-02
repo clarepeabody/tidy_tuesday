83c80dc53a8b833c4bdd93f2c41571c8e783bcd7
clarepeabody <clare.peabody@gmail.com>
2018-10-02 10:25:20 -0700

    RStudio commit 10/2

--- cats_and_dogs.R
+++ cats_and_dogs.R
@@ -1,8 +1,22 @@
 library(tidyverse)
 library(httr)
 library(readxl)
+library(usmap)
 
 GET("https://query.data.world/s/qage5daw5py3mwznddsotjbf2ewr64", write_disk(tf <- tempfile(fileext = ".xlsx")))
-pets <- read_excel(tf)
 
-head(pets)
+df <- read_excel(tf)
+
+pets <-
+  df %>%
+  mutate(ratio = `Dog Owning Households (1000s)`/`Cat Owning Households`) %>%
+  mutate(fips = fips(Location)) %>%
+  mutate(cut = cut(ratio, breaks = seq(0,2,by = 0.25), include.lowest = T))%>%
+  select(fips, ratio, cut)
+
+png("catdog.png", res = 300, width = 6*300, height = 3*300)
+plot_usmap(regions = "states", data = pets, value = "cut", lines = "white")+
+  scale_fill_viridis_d(name = "Ratio of Dog:Cat Owning Households", option = "D")+
+  labs(title = "Cats and Dogs in the U.S.")+
+  theme(legend.position = "right")
+dev.off()
